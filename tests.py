5f3bb2ea2eda6e5d9ead05a9f7bb3f7d63149798
Dag Odenhall <dag.odenhall@gmail.com>
2010-09-28 00:34:28 +0200

    Add test for entries and make that work

--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from nose.tools import istest as test
+from nose import main
 
 from jbo import Entry
 import jbo
@@ -14,3 +16,13 @@ def dbfilter():
     assert 'donri' in jbo.dbfilter(['daytime'])
     assert len(list(jbo.dbfilter('is at the on'.split()))) == 14
     assert list(jbo.dbfilter(['hello'])) == ['rinsa', 'coi']
+
+
+@test
+def entries():
+    with jbo.dbopen('entries') as db:
+        assert db[b'donri'].affixes == ['dor', "do'i"]
+
+
+if __name__ == '__main__':
+    main()
